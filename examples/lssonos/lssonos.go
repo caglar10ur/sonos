cbcd650225eba7dcc10fc578bb7b13fadf51f1f8
Çağlar Onur <caglar@10ur.org>
2023-02-04 14:40:12 -0800

    Tidy up examples a bit
    Signed-off-by: Çağlar Onur <caglar@10ur.org>

--- examples/lssonos/lssonos.go
+++ examples/lssonos/lssonos.go
@@ -6,24 +6,55 @@ import (
 	"time"
 
 	"github.com/caglar10ur/sonos"
+	avtransport "github.com/caglar10ur/sonos/services/AVTransport"
 )
 
 func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
-	son, err := sonos.NewSonos()
+	s, err := sonos.NewSonos()
 	if err != nil {
 		fmt.Printf("Error: %v\n", err)
 		return
 	}
-	defer son.Close()
+	defer s.Close()
 
-	f := func(sonos *sonos.Sonos, player *sonos.ZonePlayer) {
+	f := func(s *sonos.Sonos, player *sonos.ZonePlayer) {
 		fmt.Printf("%s\t%s\t%s\n", player.RoomName(), player.ModelName(), player.SerialNumber())
+
+		az, err := player.AVTransport.GetPositionInfo(&avtransport.GetPositionInfoArgs{})
+		if err != nil {
+			fmt.Printf("%s", err)
+			return
+
+		}
+
+		if len(az.TrackMetaData) == 0 {
+			return
+		}
+
+		metadata, err := sonos.ParseDIDL(az.TrackMetaData)
+		if err != nil {
+			fmt.Printf("%s", err)
+			return
+		}
+
+		fmt.Printf("### Now playing ###\n")
+		for _, m := range metadata.Item {
+			if len(m.Title) > 0 {
+				fmt.Printf("Title: %s\n", m.Title[0].Value)
+			}
+			if len(m.Album) > 0 {
+				fmt.Printf("Album: %s\n", m.Album[0].Value)
+			}
+			if len(m.Creator) > 0 {
+				fmt.Printf("Creator: %s\n\n", m.Creator[0].Value)
+			}
+		}
 	}
 
-	err = son.Search(ctx, f)
+	err = s.Search(ctx, f)
 	if err != nil {
 		fmt.Printf("Error: %v\n", err)
 	}
